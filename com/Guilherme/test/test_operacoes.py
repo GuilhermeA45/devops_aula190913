51a484f57096f85cb87a19d9d7d95dc79efb732a
Guilherme Alves dos Santos <1900785@SALAS.AULAS>
2019-09-13 19:51:17 -0300

    Atualizações

---
+++ com/Guilherme/test/test_operacoes.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from com.Guilherme.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+
+	def setUp(self):
+		self.operacoes = Operacoes()
+	
+	def test_soma(self):
+		self.assertEqual(self.operecoes.soma([1, 5]), 6, 'Deveria ser 6')
