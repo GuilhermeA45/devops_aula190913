cc8da93f4f2385dfcb4aeedd37e9770ccef3b641
Guilherme Alves dos Santos <1900785@SALAS.AULAS>
2019-09-13 20:03:25 -0300

    Again

--- com/Guilherme/test/test_operacoes.py
+++ com/Guilherme/test/test_operacoes.py
@@ -6,5 +6,5 @@ class TestOperacoes(TestCase):
 	def setUp(self):
 		self.operacoes = Operacoes()
 	
-	def test_soma(self):
+	def soma(self):
 		self.assertEqual(self.operecoes.soma([1, 5]), 6, 'Deveria ser 6')
